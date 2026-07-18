43a919bafb6dba556646dd397720c1c237687bd9
Selemani Lusukama Selestin <lusukamaselemani@gmail.com>
2026-07-18 05:05:11 +0200

    Insert sample course data

---
+++ sql/InsertCourses.sql
@@ -0,0 +1,7 @@
+INSERT INTO Courses
+(CourseCode, CourseName, Credits)
+VALUES
+('DB101','Database Fundamentals',20),
+('PRG101','Programming Fundamentals',20),
+('NET101','Networking Basics',15),
+('WEB101','Web Development',20);
