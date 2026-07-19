c9d655726fad4fb54638978f6cb2d72e6a486f95
Selemani Lusukama Selestin <lusukamaselemani@gmail.com>
2026-07-19 06:08:59 +0200

    Implement SQL transactions

--- sql/Transactions.sql
+++ sql/Transactions.sql
@@ -1,3 +1,43 @@
 USE SchoolDB
 
 GO
+
+/* Transactions allow us to treat several SQL statements as one unit of work. */
+
+BEGIN TRANSACTION;
+
+INSERT INTO Students
+(
+StudentNumber,
+FirstName,
+LastName,
+DateOfBirth,
+Email,
+IsActive
+)
+
+VALUES
+(
+'ST1006',
+'Aruna',
+'Lusukama',
+'2004-10-20',
+'aruna.l@ctucareer.co.za',
+1
+);
+
+/* display student */
+
+SELECT * FROM Students
+
+/* Rollback class (undo) */
+
+ROLLBACK;
+
+/* Display students */
+
+SELECT * FROM Students
+
+/*
+Note : Rollback undo changes commit 
+*/
