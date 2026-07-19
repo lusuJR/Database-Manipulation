32af80a598445312377f635883870f00f7a209dd
Selemani Lusukama Selestin <lusukamaselemani@gmail.com>
2026-07-19 12:58:54 +0200

    Configure successful connection to SchoolDB

--- Database-Manipulation/SchoolDB.ConsoleApp/Program.cs
+++ Database-Manipulation/SchoolDB.ConsoleApp/Program.cs
@@ -1,1 +1,23 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using Microsoft.Data.SqlClient;
+
+string connectionString =
+    "Server=.;" +
+    "Database=SchoolDB;" +
+    "Trusted_Connection=True;" +
+    "TrustServerCertificate=True;";
+
+try
+{
+    Console.WriteLine("Connecting to SchoolDB...");
+
+    using SqlConnection connection = new(connectionString);
+
+    connection.Open();
+
+    Console.WriteLine("Connected successfully!");
+}
+catch (Exception ex)
+{
+    Console.WriteLine("Connection failed.");
+    Console.WriteLine(ex.Message);
+}
